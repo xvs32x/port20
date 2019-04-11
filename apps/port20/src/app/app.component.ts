89926c56999661fe9ef71eb940a0c65765a83003
NagSer <x64vs32x@mail.ru>
2019-04-12 00:07:45 +0300

    body image && animation for navs

--- apps/port20/src/app/app.component.ts
+++ apps/port20/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { Component } from "@angular/core";
   selector: "port20-root",
   template: `
     <div class="uk-cover-container" uk-height-viewport>
-      <video class="uk-animation-fade" autoplay loop muted playsinline uk-cover>
+      <img class="uk-animation-fade" src="assets/images/body.jpg" alt="" uk-cover>
+      <video class="uk-animation-fade"  autoplay loop muted playsinline uk-cover>
         <source src="assets/videos/body.mp4" type="video/mp4">
       </video>
-      <div>
-        <port20-nav-bar></port20-nav-bar>
+      <div class="uk-flex uk-flex-column" uk-height-viewport>
+        <port20-nav-bar class="uk-display-block uk-animation-slide-bottom"></port20-nav-bar>
         <router-outlet></router-outlet>
-        <port20-footer></port20-footer>
+        <port20-footer class="uk-display-block uk-animation-slide-top"></port20-footer>
       </div>
     </div>
   `
--- apps/port20/src/app/components/footer/footer.component.ts
+++ apps/port20/src/app/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
     <div class="uk-container uk-container-small uk-flex uk-flex-center uk-position-relative uk-margin-top">
       <div class="pyramid uk-width-small uk-text-center">
         <a routerLink="/" class="uk-display-block uk-margin-top" href="">
-          <span uk-icon="icon: github; ratio: 1.5"></span>
+          <span uk-icon="icon: github-alt; ratio: 1.5"></span>
         </a>
       </div>
     </div>
